bb43d86c219e479c3808ffc1fbff797e930ebc99
yjgaia <yjdev88@gmail.com>
2025-02-02 14:49:49 +0900

    Add TypeScript definitions for various expression types including literals, function, call, if, while, and comment expressions

--- lib/code/Expression.d.ts
+++ lib/code/Expression.d.ts
@@ -1,8 +1,46 @@
+interface BooleanLiteral {
+    type: "boolean";
+    value: boolean;
+}
+interface NumberLiteral {
+    type: "number";
+    value: string;
+}
+interface StringLiteral {
+    type: "string";
+    value: string;
+}
 interface VarExpression {
-    type: "VarExpression";
+    type: "var";
     name: string;
     init: Expression;
 }
-type Expression = VarExpression;
+interface FunctionExpression {
+    type: "function";
+    name: string;
+    params: string[];
+    body: Expression[];
+}
+interface CallExpression {
+    type: "call";
+    callee: Expression;
+    args: Expression[];
+}
+interface IfExpression {
+    type: "if";
+    test: Expression;
+    then: Expression[];
+    else?: Expression[];
+}
+interface WhileExpression {
+    type: "while";
+    test: Expression;
+    body: Expression[];
+}
+interface CommentExpression {
+    type: "comment";
+    text: string;
+}
+type Expression = BooleanLiteral | NumberLiteral | StringLiteral | VarExpression | FunctionExpression | CallExpression | IfExpression | WhileExpression | CommentExpression;
 export default Expression;
 //# sourceMappingURL=Expression.d.ts.map
